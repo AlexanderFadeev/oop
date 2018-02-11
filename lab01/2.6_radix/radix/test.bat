10a8018ffaa9e31f0720ca4b169511ff52a34cea
AlexanderFadeev <1alexking1@gmail.com>
2018-02-11 21:24:08 +0300

    Added new test cases to Radix

--- lab01/2.6_radix/radix/test.bat
+++ lab01/2.6_radix/radix/test.bat
@@ -10,8 +10,8 @@ REM Hex to MAXINT32
 CALL :run_test 2 16 10 7FFFFFFF
 IF ERRORLEVEL 1 GOTO err
 
-REM Hex to MAXINT32 overflow
-CALL :run_bad_test 3 16 10 80000000
+REM Hex to MININT32
+CALL :run_test 3 16 10 -80000000
 IF ERRORLEVEL 1 GOTO err
 
 REM Radix 36 to binary
@@ -20,6 +20,18 @@ IF ERRORLEVEL 1 GOTO err
 
 REM Decimal to binary
 CALL :run_test 5 10 2 32767
+IF ERRORLEVEL 1 GOTO err
+
+REM Hex to MAXINT32+1
+CALL :run_test_fail 16 10 80000000
+IF ERRORLEVEL 1 GOTO err
+
+REM Hex to MININT32-1
+CALL :run_test_fail 16 10 -80000001
+IF ERRORLEVEL 1 GOTO err
+
+REM Wrong digits for a given radix
+CALL :run_test_fail 2 10 12345
 IF ERRORLEVEL 1 GOTO err
 
 echo "PASS"
@@ -33,12 +45,10 @@ fc %TEMP%\output%ID%.txt tests/expected%ID%.txt
 IF ERRORLEVEL 1 EXIT /B 1
 EXIT /B 0
 
-:run_bad_test
-SET ID=%~1
-%PROGRAM% "%~2" "%~3" "%~4" >%TEMP%\output%ID%.txt
-fc %TEMP%\output%ID%.txt tests/expected%ID%.txt
-IF ERRORLEVEL 1 EXIT /B 1
-EXIT /B 0
+:run_test_fail
+%PROGRAM% "%~1" "%~2" "%~3" >%TEMP%\output.txt
+IF ERRORLEVEL 1 EXIT /B 0
+EXIT /B 1
 
 
 :err

c9d8cfe42d3403aee522fec32a20c5675e029d75
AlexanderFadeev <1alexking1@gmail.com>
2018-03-15 05:26:36 +0300

    Add 6.1-Expand Template

---
+++ lab02/6.1_expand_template/expand_template/main.cpp
@@ -0,0 +1,20 @@
+#include <exception>
+#include <iostream>
+
+int main()
+{
+	try
+	{
+		//TODO
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	catch (...)
+	{
+		std::cerr << "Unknown exception" << std::endl;
+		return 1;
+	}
+}

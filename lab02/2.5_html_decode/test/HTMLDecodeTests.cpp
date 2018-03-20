bc14bbabeb9a15da78de6d729510c65459137ca2
AlexanderFadeev <1alexking1@gmail.com>
2018-03-20 23:30:24 +0300

    HTML Decode: Add test cases

--- lab02/2.5_html_decode/test/HTMLDecodeTests.cpp
+++ lab02/2.5_html_decode/test/HTMLDecodeTests.cpp
@@ -1,2 +1,35 @@
 #include "../html_decode/HTMLDecode.hpp"
 #include "catch.hpp"
+
+TEST_CASE("Empty text decodes into empty text", "[decode]")
+{
+	CHECK(HtmlDecode("") == "");
+}
+
+TEST_CASE("Text without escape sequences does not change", "[decode]")
+{
+	std::string text("Hello, World!");
+	CHECK(HtmlDecode(text) == text);
+}
+
+TEST_CASE("All possible escape characters are recognized", "[decode]")
+{
+	CHECK(HtmlDecode(R"(Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s)") == R"(Cat <says> "Meow". M&M's)");
+}
+
+TEST_CASE("Unknown escape characters remain the same", "[decode]")
+{
+	CHECK(HtmlDecode("&kek;") == "&kek;");
+	CHECK(HtmlDecode("&copy;") == "&copy;");
+}
+
+TEST_CASE("Only escaped sequences are changed", "[decode]")
+{
+	CHECK(HtmlDecode("amp;&amp") == "amp;&amp");
+}
+
+TEST_CASE("Decodes properly when ampersand was met earlier", "[decode]")
+{
+	CHECK(HtmlDecode("&amp&amp;") == "&amp&");
+	CHECK(HtmlDecode("&&amp;") == "&&");
+}

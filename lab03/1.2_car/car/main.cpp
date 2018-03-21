0b52ec1c48971bd7b7ae4e35b857bda30af13ecb
AlexanderFadeev <1alexking1@gmail.com>
2018-03-21 14:06:09 +0300

    Car: Create user input handler prototype

---
+++ lab03/1.2_car/car/main.cpp
@@ -0,0 +1,113 @@
+#include "Car.hpp"
+#include <exception>
+#include <iostream>
+#include <map>
+#include <string>
+
+enum class Command
+{
+	Info,
+	EngineOn,
+	EngineOff,
+	SetGear,
+	SetSpeed,
+	Unknown,
+};
+
+const std::map<std::string, Command> STRING_TO_COMMAND{
+	{ "Info", Command::Info },
+	{ "EngineOn", Command::EngineOn },
+	{ "EngineOff", Command::EngineOff },
+	{ "SetGear", Command::SetGear },
+	{ "SetSpeed", Command::SetSpeed },
+};
+
+Command ParseCommand(const std::string& str)
+{
+	auto& mapping = STRING_TO_COMMAND;
+	if (mapping.find(str) == mapping.end())
+	{
+		return Command::Unknown;
+	}
+	return mapping.at(str);
+}
+
+bool CommandNeedsParameter(Command command)
+{
+	return command == Command::SetGear || command == Command::SetSpeed;
+}
+
+void RunCommand(std::ostream& output, Car& car, Command command, int parameter)
+{
+	switch (command)
+	{
+	case Command::SetGear:
+		break;
+	case Command::SetSpeed:
+		break;
+	default:
+		throw std::logic_error("Unknown command with parameter");
+	}
+}
+
+void RunCommand(std::ostream& output, Car& car, Command command)
+{
+	switch (command)
+	{
+	case Command::Info:
+		break;
+	case Command::EngineOn:
+		break;
+	case Command::EngineOff:
+		break;
+	default:
+		throw std::logic_error("Unknown command without parameter");
+	}
+}
+
+void HandleCommands(std::istream& input, std::ostream& output, Car& car)
+{
+	std::string commandStr;
+	while (input >> commandStr)
+	{
+		auto command = ParseCommand(commandStr);
+		if (command == Command::Unknown)
+		{
+			continue;
+		}
+
+		if (!CommandNeedsParameter(command))
+		{
+			RunCommand(output, car, command);
+			continue;
+		}
+
+		int param;
+		if (!(input >> param))
+		{
+			continue;
+		}
+
+		RunCommand(output, car, command, param);
+	}
+}
+
+int main()
+{
+	try
+	{
+		Car car;
+		HandleCommands(std::cin, std::cout, car);
+		return 0;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	catch (...)
+	{
+		std::cerr << "Unknown exception" << std::endl;
+		return 1;
+	}
+}

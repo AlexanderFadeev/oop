b8d28c25207f78a87cc74711379b4d85c3a8227c
AlexanderFadeev <1alexking1@gmail.com>
2018-03-21 10:19:29 +0300

    HTML Decode: Update tests

--- lab02/2.5_html_decode/test/HTMLDecodeTests.cpp
+++ lab02/2.5_html_decode/test/HTMLDecodeTests.cpp
@@ -6,24 +6,24 @@ TEST_CASE("Empty text decodes into empty text", "[decode]")
 	CHECK(HtmlDecode("") == "");
 }
 
-TEST_CASE("Text without escape sequences does not change", "[decode]")
+TEST_CASE("Text without html entities does not change", "[decode]")
 {
 	std::string text("Hello, World!");
 	CHECK(HtmlDecode(text) == text);
 }
 
-TEST_CASE("All possible escape characters are recognized", "[decode]")
+TEST_CASE("All possible html entities are recognized", "[decode]")
 {
 	CHECK(HtmlDecode(R"(Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s)") == R"(Cat <says> "Meow". M&M's)");
 }
 
-TEST_CASE("Unknown escape characters remain the same", "[decode]")
+TEST_CASE("Unknown entities remain the same", "[decode]")
 {
 	CHECK(HtmlDecode("&kek;") == "&kek;");
 	CHECK(HtmlDecode("&copy;") == "&copy;");
 }
 
-TEST_CASE("Only escaped sequences are changed", "[decode]")
+TEST_CASE("Only escaped entities are changed", "[decode]")
 {
 	CHECK(HtmlDecode("amp;&amp") == "amp;&amp");
 }
@@ -32,4 +32,9 @@ TEST_CASE("Decodes properly when ampersand was met earlier", "[decode]")
 {
 	CHECK(HtmlDecode("&amp&amp;") == "&amp&");
 	CHECK(HtmlDecode("&&amp;") == "&&");
+}
+
+TEST_CASE("Does not decode already decoded characters", "[decode]")
+{
+	CHECK(HtmlDecode("&amp;lt;") == "&lt;");
 }

d2ce85412bbd023417d3a884b90a4a084f3f9d6c
AlexanderFadeev <1alexking1@gmail.com>
2018-05-11 19:27:09 +0300

    FindMax: Move implementation to .ipp file

--- lab07/1.3_find_max_ex/find_max_ex/FindMax.hpp
+++ lab07/1.3_find_max_ex/find_max_ex/FindMax.hpp
@@ -4,29 +4,6 @@
 #include <vector>
 
 template <typename T, typename Less = std::less<T>>
-bool FindMax(const std::vector<T>& vec, T& maxValue, const Less& less = std::less<T>()) try
-{
-	if (vec.empty())
-	{
-		return false;
-	}
+bool FindMax(const std::vector<T>& vec, T& maxValue, const Less& less = std::less<T>());
 
-	size_t maxIndex = 0;
-	auto greater = std::not_fn(less);
-	for (size_t index = 1; index < vec.size(); index++)
-	{
-		auto& max = vec[maxIndex];
-		auto& value = vec[index];
-		if (greater(value, max))
-		{
-			maxIndex = index;
-		}
-	}
-
-	maxValue = vec[maxIndex];
-	return true;
-}
-catch (...)
-{
-	return false;
-}
+#include "FindMax.ipp"
---
+++ lab07/1.3_find_max_ex/find_max_ex/FindMax.ipp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "FindMax.hpp"
+
+template <typename T, typename Less>
+bool FindMax(const std::vector<T>& vec, T& maxValue, const Less& less) try
+{
+	if (vec.empty())
+	{
+		return false;
+	}
+
+	size_t maxIndex = 0;
+	auto greater = std::not_fn(less);
+	for (size_t index = 1; index < vec.size(); index++)
+	{
+		auto& max = vec[maxIndex];
+		auto& value = vec[index];
+		if (greater(value, max))
+		{
+			maxIndex = index;
+		}
+	}
+
+	maxValue = vec[maxIndex];
+	return true;
+} catch (...)
+{
+	return false;
+}

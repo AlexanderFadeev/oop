976f4f4f4f79c0ceb2b621d5411ad88244664d4d
AlexanderFadeev <1alexking1@gmail.com>
2018-05-19 11:12:16 +0300

    HTTP URL: Implement controller

--- lab06/1.7_http_url/http_url/main.cpp
+++ lab06/1.7_http_url/http_url/main.cpp
@@ -1,19 +1,47 @@
+#include "HttpUrl.hpp"
 #include <exception>
 #include <iostream>
+#include <string>
+
+void PrintURLInfo(std::ostream& output, const CHttpUrl& url)
+{
+	output << "HOST: " << url.GetDomain() << '\n'
+		<< "PORT: " << url.GetPort() << '\n'
+		<< "DOC: " << url.GetDocument() << std::endl;
+
+	if (!output)
+	{
+		throw std::runtime_error("Failed to write to output stream");
+	}
+}
+
+void HandleURLs(std::istream& input, std::ostream& output)
+{
+	std::string line;
+	while (std::getline(input, line))
+	{
+		if (line.empty())
+		{
+			continue;
+		}
+
+		try
+		{
+			CHttpUrl url(line);
+			PrintURLInfo(output, url);
+		} 
+		catch (const std::exception& e)
+		{
+			std::cerr << "Exception: " << e.what() << std::endl;
+		} 
+		catch (...)
+		{
+			std::cerr << "Unknown exception" << std::endl;
+		}
+	}
+}
 
 int main()
 {
-	try
-	{
-		// TODO
-		return 0;
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << std::endl;
-	}
-	catch (...)
-	{
-		std::cerr << "Unknown exception" << std::endl;
-	}
+	HandleURLs(std::cin, std::cout);
 }
